bc8e222e35b2a54f528347becea679010d808b5c
Anurag Rawat <anurag.rawat0201@gmail.com>
2025-04-21 18:56:07 +0100

    setup example data-table

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core'
 import { TableComponent } from 'table'
 import { ColumnDef } from '@tanstack/angular-table'
-
-type Person = {
-  firstName: string
-  lastName: string
-  age: number
-  visits: number
-  status: string
-  progress: number
-}
+import { type Transaction, transactions } from './data'
 
 @Component({
   imports: [TableComponent],
@@ -18,67 +10,39 @@ type Person = {
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app'
-
-  data: Person[] = [
+  columns: ColumnDef<Transaction>[] = [
     {
-      firstName: 'tanner',
-      lastName: 'linsley',
-      age: 24,
-      visits: 100,
-      status: 'In Relationship',
-      progress: 50,
-    },
-    {
-      firstName: 'tandy',
-      lastName: 'miller',
-      age: 40,
-      visits: 40,
-      status: 'Single',
-      progress: 80,
-    },
-    {
-      firstName: 'joe',
-      lastName: 'dirte',
-      age: 45,
-      visits: 20,
-      status: 'Complicated',
-      progress: 10,
-    },
-  ]
-
-  columns: ColumnDef<Person>[] = [
-    {
-      accessorKey: 'firstName',
+      accessorKey: 'transaction_id',
       cell: (info) => info.getValue(),
+      header: 'Transaction ID',
+    },
+    {
+      accessorKey: 'transaction_date',
+      id: 'date',
+      cell: (info) => info.getValue(),
+      header: 'Date',
+    },
+    {
+      accessorKey: 'expense_status',
+      header: () => 'Expense Status',
+    },
+    {
+      accessorKey: 'payment_status',
+      header: () => `<span>Payment</span>`,
       footer: (info) => info.column.id,
     },
     {
-      accessorFn: (row) => row.lastName,
-      id: 'lastName',
-      cell: (info) => `<i>${info.getValue<string>()}</i>`,
-      header: (context) => `<span>Last Name ${context}</span>`,
+      accessorKey: 'category',
+      header: 'Category',
       footer: (info) => info.column.id,
     },
     {
-      accessorKey: 'age',
-      header: () => 'Age',
+      accessorKey: 'amount',
+      header: 'Amount ($)',
       footer: (info) => info.column.id,
-    },
-    {
-      accessorKey: 'visits',
-      header: () => `<span>Visits</span>`,
-      footer: (info) => info.column.id,
-    },
-    {
-      accessorKey: 'status',
-      header: 'Status',
-      footer: (info) => info.column.id,
-    },
-    {
-      accessorKey: 'progress',
-      header: 'Profile Progress',
-      footer: (info) => info.column.id,
+      meta: {},
     },
   ]
+
+  protected readonly transactions = transactions
 }
